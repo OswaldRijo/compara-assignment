4466d57d3e6cadc4cc8a041393db6e2577f35013
Oswald <oswald.rijo@gmail.com>
2021-02-22 10:35:17 -0300

    adding Common Rule test

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -6,10 +6,27 @@ const Product = coTest.Product;
 
 describe("Co Test", function() {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
-  });
+  describe('Common Coverage', ()=>{
+    it("should decrease sellIn and price", ()=>{
+      const carInsurance = new CarInsurance([ new Product('Medium Coverage', 10, 20) ]);
+      const products = carInsurance.updatePrice()
+      expect(products[0].sellIn).equal(9);
+      expect(products[0].price).equal(19);
+    })
+    it("should decrease sellIn and price twice faster", ()=>{
+      const carInsurance = new CarInsurance([ new Product('Medium Coverage', 0, 20) ]);
+      const products = carInsurance.updatePrice()
+      expect(products[0].sellIn).equal(-1);
+      expect(products[0].price).equal(18);
+    })
+
+    it("should decrease sellIn and should not decrease price", ()=>{
+      const carInsurance = new CarInsurance([ new Product('Medium Coverage', -1, 0) ]);
+      const products = carInsurance.updatePrice()
+      expect(products[0].sellIn).equal(-2);
+      expect(products[0].price).equal(0);
+    })
+  })
+
 
 });
